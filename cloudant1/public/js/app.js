d0c10df20d75d519bc498f96ee18251c21c76722
jpiovar <jpiovar@sk.ibm.com>
2017-03-21 16:36:23 +0100

    cloudant db

---
+++ cloudant1/public/js/app.js
@@ -0,0 +1,209 @@
+var meanApp = angular.module('meanApp', ['ngRoute']);
+
+
+meanApp.config(function($routeProvider,$locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    $routeProvider
+    .when("/", {
+        templateUrl : "partials/home.html",
+        controller : "appCtrl"
+    })
+    .when("/city", {
+        templateUrl : "partials/city.html",
+        controller : "cityCtrl"
+    })
+    .when("/town", {
+        templateUrl : "partials/town.html",
+        controller : "townCtrl"
+    })
+    .when("/list", {
+        templateUrl : "partials/list.html",
+        controller : "listCtrl"
+    })
+    .otherwise({ redirectTo: '/' });
+});
+
+meanApp.controller('appCtrl', function($http) {
+    var apc = this;
+    var url = "http://localhost:3030";
+
+    apc.thingsFromMongo = [];
+
+    apc.submit = function(thing){
+      console.log('apc submited');
+       $http.post(url + '/add', {thingInput: thing})
+       .then(function(response) {
+            console.log("Server response ");
+            apc.respost = response.data.a;
+        });
+       /*.success(function(data, status, headers, config){
+        apc.respost = data;
+       })
+       .error(function(data, status, headers, config){
+        apc.respost = status;
+      });*/
+
+
+
+    }
+
+
+
+});
+
+
+
+meanApp.controller('townCtrl', function($http) {
+    var town = this;
+    var url = "http://localhost:3030";
+
+    town.townButtonClick = function(townItem){
+      debugger;
+      console.log('town button clicked');
+       $http.post(url + '/townAdd', {townItemClient: townItem})
+       .then(function(response) {
+            console.log("Server response ");
+            town.responsePost = response.data.townA;
+        });
+    }
+
+
+});
+
+
+
+meanApp.service('mongoDb', function($http) { debugger;
+    var self = this;
+    var url = "http://localhost:3030";
+
+    self.getRecord = function () {
+      console.log('Get record from mongodb');
+      return $http.post(url + '/getRecord', {action: 'getRecord'})
+       .then(function(response) {
+
+            console.log("Server response");debugger;
+            return response.data.responseAction;
+
+      });
+    }
+
+    self.addRecord = function(recordInput){
+      console.log('Add record to mongodb');
+      return $http.post(url + '/addRecord', {action: 'addRecord', recordAddClient: recordInput})
+       .then(function(response) {
+            console.log("Server response");
+            //self.responseAction = response.data.responseAddRecord;
+            //self.getAllDataDb();
+            return response.data.responseAction;
+      });
+    }
+
+    self.removeRecord = function(recordId){debugger;
+      console.log('Remove record from mongodb');
+      return $http.post(url + '/removeRecord', {action: 'removeRecord', recordRemoveClient: recordId})
+       .then(function(response) {
+            console.log("Server response");
+            //self.responseAction = response.data.responseAddRecord;
+            //self.getAllDataDb();
+            return response.data.responseAction;
+      });
+    }
+
+    self.updateRecord = function(id,val){debugger;
+      console.log('Update record from mongodb');
+      return $http.post(url + '/updateRecord', {action: 'updateRecord', recordUpdateClient: {'id':id,'val':val} })
+       .then(function(response) {
+            console.log("Server response");
+            //self.responseAction = response.data.responseAddRecord;
+            //self.getAllDataDb();
+            return response.data.responseAction;
+      });
+    }
+
+});
+
+meanApp.controller('listCtrl', function($http, mongoDb) {
+    var self = this;
+    var url = "http://localhost:3030";
+
+
+    self.getAllDataDb = function(){ debugger;
+      mongoDb.getRecord().then(function(data){
+          self.items = data;
+       });
+       debugger;
+    }
+
+
+
+    /*
+    self.getAllDataDb = function(){
+
+      console.log('Get record from mongodb');
+      $http.post(url + '/getAllRecord', {recordAddClient: self.recordInput})
+       .then(function(response) {
+            console.log("Server response");
+            self.responseAction = response.data.responseAddRecord;
+      });
+
+    }
+    */
+
+    self.addRecord = function(){
+
+      console.log('Add record button clicked');
+      mongoDb.addRecord(self.recordInput).then(function(data){debugger;
+          self.items = data;
+       });
+       debugger;
+      /*
+      $http.post(url + '/addRecord', {recordAddClient: self.recordInput})
+       .then(function(response) {debugger;
+            console.log("Server response");
+            //self.responseAction = response.data.responseAddRecord;
+            //self.getAllDataDb();
+            self.items = response.data.responseAction;
+      });
+      */
+    }
+
+
+
+    self.removeRecord = function(id){ debugger;
+      console.log('Remove record button clicked');
+      mongoDb.removeRecord(id).then(function(data){debugger;
+          self.items = data;
+       });
+       debugger;
+    }
+
+
+    self.updateRecord = function(id,val){ debugger;
+      console.log('Update record button clicked');
+      mongoDb.updateRecord(id,val).then(function(data){debugger;
+          self.items = data;
+       });
+       debugger;
+    }
+
+
+    self.getInitItem = function(item){debugger;
+      return item;
+    }
+
+
+});
+
+meanApp.directive('todoLine',function(){
+  return {
+    restrict : "A",
+    scope: false,
+    transclude: true,
+    templateUrl: 'partials/todoLineTemplate.html'
+    //template: '<td>{{item.item}}</td><td><button type="button" class="btn btn-default" ng-click="list.removeRecord(item.id)">Remove</button></td>'    
+  }
+});
+
---
+++ cloudant1/server.js
@@ -0,0 +1,568 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+
+
+var Cloudant = require('cloudant');
+
+
+if(Cloudant){
+  console.log('cloudant aaaaaa11111111111');
+}
+
+// Initialize 
+var cloudant = Cloudant({ url: "https://1fb4d114-f376-4d3e-a8d9-ed8770e944e2-bluemix:2df794d6637c9ab0d7a12a1d9937b703d24ab4c191da332dba88127b24dd4591@1fb4d114-f376-4d3e-a8d9-ed8770e944e2-bluemix.cloudant.com"}, function(err, data) {
+  console.log(err,data);
+}); 
+ 
+ 
+cloudant.db.list(function(err, allDbs) {
+  console.log('All my databases: %s', allDbs.join(', '))
+});
+
+
+var db = cloudant.db.use("db1");
+db.list(function(err, data){
+   console.log(err,data);
+   console.log('db1',data);
+});
+
+db.insert({ crazy: true }, 'rabbit', function(err, body, header) {
+  if (err) {
+    return console.log('[db.insert] ', err.message);
+  }
+
+  console.log('You have inserted the rabbit.');
+  console.log(body);
+});
+
+
+
+
+
+
+//var $ = jQuery = require('jquery')(window);
+
+var cors = require('cors');
+
+var Promise = require("native-promise-only");
+
+var p = new Promise(function(resolve,reject){
+    var a = "aaaaaaaaaaaaaaa11111";
+    //setTimeout(function(){
+        resolve(a);
+    //},5000);
+});
+
+var p1 = new Promise(function(resolve,reject){
+    var a = "oooookkkkk";
+    setTimeout(function(){
+        resolve(a);
+    },5000);
+});
+
+var p3 = function(){
+   return 'bbb';
+}
+
+p.then(function(msg){
+    console.log(msg); // Yay!
+}).then(function(){
+    console.log('aaaaa222222'); // Yay!
+}).then(function(){
+    console.log(p3());
+});
+
+p1.then(function(jj){console.log(jj);});
+
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+var colectionMongoDb = 'Record';
+
+
+
+
+
+
+
+var addRecordMongo = function(collectionName,record,res){
+
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+	     if(err) throw err;
+
+	     db.collection(collectionName, function (err, collection) {
+	        collection.insert({ item: record }, function(err, r) {
+		          db.close();
+              getRecord(res);
+	        });
+	     });
+    });
+
+
+}
+
+
+
+
+function getRecordMongo(collectionName){
+
+  MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+    if(err) throw err;
+
+    db.collection('Record', function (err, collection) {
+      collection.find().toArray(function(err, items) {
+        if(err) throw err;
+        console.log('service items from mongodb '+items);
+
+        setTimeout(function(){
+          return items;
+        },5000);
+        db.close();
+        /*
+        res.setHeader('content-type', 'application/json');
+        res.json({ 'responseAction': result });
+        */
+      });
+    });
+  });
+
+
+
+  //db.collection('Record', function (err, collection) {
+  //
+  //     collection.find().toArray(function(err, items) {
+  //         if(err) throw err;
+  //         console.log(items);
+  //         mongoMessage = items.length;
+  //     });
+  //
+  //   });
+}
+
+
+
+
+
+// MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+//
+//   if(err) throw err;
+//
+//
+//   db.collection('Record', function (err, collection) {
+//
+//     collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+//     collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+//     collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+//
+//   });
+//
+//
+//   db.collection('Record').count(function (err, count) {
+//
+//     if (err) throw err;
+//     console.log('Total Rows: ' + count);
+//
+//   });
+//
+//
+//   db.collection('Record', function (err, collection) {
+//
+//     collection.update({id: 1}, { $set: { firstName: 'James', lastName: 'Gosling'} }, {w:1}, function(err, result){
+//       if(err) throw err;
+//       console.log('Document Updated Successfully');
+//     });
+//
+//   });
+//
+//
+//   db.collection('Record', function (err, collection) {
+//
+//     collection.remove({id:2}, {w:1}, function(err, result) {
+//         if(err) throw err;
+//         console.log('Document Removed Successfully');
+//     });
+//
+//   });
+//
+//
+//   db.collection('Record', function (err, collection) {
+//
+//     collection.find().toArray(function(err, items) {
+//         if(err) throw err;
+//         console.log(items);
+//         mongoMessage = items.length;
+//     });
+//
+//   });
+//
+//
+//
+// });
+
+
+
+
+
+
+
+
+var app = express();
+
+app.use(cors());
+//app.use(bodyParser());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+//app.use( express.static( __dirname + '/public' ));
+app.use( express.static( __dirname + '/public'));
+
+
+
+//app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+//app.use('/node_modules', express.static('node_modules'));
+app.use( express.static( __dirname + '/node_modules'));
+
+
+app.get('*',function(req, res){
+
+     //res.sendFile( __dirname + "/" + "index.html" );
+     res.sendFile( __dirname + '/public/index.html' );
+});
+
+/*
+app.get('/city',function(req, res){
+
+     //res.sendFile( __dirname + "/" + "index.html" );
+     res.sendFile( __dirname + '/public/index.html' );
+});
+*/
+
+
+
+
+
+app.post('/add',function(req, res){
+  var thing = req.body.thingInput;
+  //var thingDoc = new thingModel({thing: thing});
+  console.log(thing);
+
+  //res.setHeader('content-type', 'application/json');
+  //res.json({ a: 1 });
+  //res.send(thing);
+  /*thing.Doc.save(function(){
+    res.send();
+  });*/
+});
+
+
+app.post('/townAdd',function(req, res){
+  var tic = req.body.townItemClient;
+
+  console.log(tic);
+
+  res.setHeader('content-type', 'application/json');
+  res.json({ townA: 'server town response' });
+
+});
+
+
+
+
+
+
+
+
+
+
+/* list past start */
+
+
+
+var getRecord = function() {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+      if(err) {
+        reject(err); return;
+      }
+
+      db.collection('Record', function (err, collection) {
+
+        collection.find().toArray(function(err, items) {
+
+          if(err) {
+            reject(err); return;
+          }
+
+          console.log('service items from mongodb '+items);
+          console.log('pr next'+items);
+          resolve(items);
+          db.close();
+
+        });
+
+      });
+
+    });
+
+  });
+
+};
+
+
+var getMaxObjArray = function(objArr) {
+  var maxObjArr = 0;
+  for(var i in objArr){
+    if(objArr[i].id > maxObjArr){
+      maxObjArr = objArr[i].id;
+    }
+  }
+  return maxObjArr;
+}
+
+var addRecord = function(collectionName,record){
+
+  return new Promise(function(resolve, reject) {
+
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+	     if(err){
+         reject(err);
+         return;
+       }
+
+
+       db.collection(collectionName, function (err, collection) {
+
+
+          collection.find().toArray(function(err, items) {
+
+            if(err) {
+              reject(err); return;
+            }
+
+            console.log('service items from mongodb '+items);
+            console.log('pr next'+items);
+
+
+            var maxRecord = getMaxObjArray(items);
+            console.log(maxRecord);
+
+            collection.insert({ id: maxRecord+1, item: record }, function(err, result) {
+
+              if(err) {
+                reject(err);
+                return;
+              }
+
+              db.close();
+              resolve(result);
+
+  	        });
+
+          });
+
+
+
+	     });
+
+
+
+
+
+
+
+
+    });
+
+  });
+
+}
+
+
+
+var removeRecord = function(collectionName,recordId){
+
+  return new Promise(function(resolve, reject) {
+
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+	     if(err){
+         reject(err);
+         return;
+       }
+
+	     db.collection(collectionName, function (err, collection) {
+
+	        collection.remove({id:recordId}, {w:1}, function(err, result) {
+            if(err) {
+              reject(err);
+              return;
+            }
+            console.log('Removed Successfully');
+
+            db.close();
+            resolve(result);
+
+          });
+
+	     });
+
+    });
+
+  });
+
+}
+
+
+
+
+var updateRecord = function(collectionName,record){
+
+  return new Promise(function(resolve, reject) {
+
+    MongoClient.connect("mongodb://localhost:27017/mean", function (err, db) {
+
+	     if(err){
+         reject(err);
+         return;
+       }
+
+	     db.collection(collectionName, function (err, collection) {
+
+
+         collection.update({id: record.id}, { $set: { item: record.val} }, {w:1},
+          function(err, result){
+            if(err) {
+              reject(err);
+              return;
+            }
+            console.log('Updated Successfully');
+
+            db.close();
+            resolve(result);
+
+          });
+
+	     });
+
+    });
+
+  });
+
+}
+
+
+
+
+app.post('/getRecord',function(req, res){
+  var actionClient = req.body.action;
+  console.log(actionClient);
+
+  getRecord().then(function(items){
+    res.setHeader('content-type', 'application/json');
+    res.json({ 'responseAction': items });
+    res.end();
+  });
+
+});
+
+
+
+app.post('/addRecord',function(req, res){
+  var actionClient = req.body.action;
+  console.log(actionClient);
+
+  var recordAddClient = req.body.recordAddClient;
+
+  console.log(recordAddClient);
+
+  addRecord(colectionMongoDb,recordAddClient)
+    .then(function(result){
+      getRecord()
+        .then(function(items){
+          res.setHeader('content-type', 'application/json');
+          //res.json({ 'responseAddRecord': ' record ' + recordAddClient + ' added to mongodb', 'newItems':items });
+          res.json({ 'responseAction': items });
+          res.end();
+        });
+    });
+
+
+
+
+});
+
+app.post('/removeRecord',function(req, res){
+  var actionClient = req.body.action;
+  console.log(actionClient);
+
+  var recordRemoveClient = req.body.recordRemoveClient;
+
+  console.log(recordRemoveClient);
+
+  removeRecord(colectionMongoDb,recordRemoveClient)
+    .then(function(result){
+
+      return getRecord();
+
+    })
+    .then(function(items){
+
+      res.setHeader('content-type', 'application/json');
+      //res.json({ 'responseAddRecord': ' record ' + recordAddClient + ' added to mongodb', 'newItems':items });
+      res.json({ 'responseAction': items });
+      res.end();
+
+    });
+
+});
+
+
+
+
+app.post('/updateRecord',function(req, res){
+  var actionClient = req.body.action;
+  console.log(actionClient);
+
+  var recordUpdateClient = req.body.recordUpdateClient;
+
+  console.log(recordUpdateClient);
+
+  updateRecord(colectionMongoDb,recordUpdateClient)
+    .then(function(result){
+
+      return getRecord();
+
+    })
+    .then(function(items){
+
+      res.setHeader('content-type', 'application/json');
+      //res.json({ 'responseAddRecord': ' record ' + recordAddClient + ' added to mongodb', 'newItems':items });
+      res.json({ 'responseAction': items });
+      res.end();
+
+    });
+
+});
+
+
+
+
+/* list part end */
+
+
+
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+console.log('Listening on port' + port + '...' + __dirname );
